1034bf3941f85e9b4b5e32ef96062fb658d8f71d
NaveenSriramAddagarla <NaveenSriramAddagarla>
2024-11-25 21:36:23 +0000

    TD Updates

---
+++ Databricks/ARCHIVE/TD/ARIA_TD_ADLS_TO_EVENTHUBS.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Tribunal Decision Archive from Gold to Event Hubs
+# MAGIC <table style='float:left;'>
+# MAGIC    <tbody>
+# MAGIC       <tr>
+# MAGIC          <td style='text-align: left;'><b>Name: </b></td>
+# MAGIC          <td>ARIA_TD_ADLS_TO_EVENTHUBS</td>
+# MAGIC       </tr>
+# MAGIC       <tr>
+# MAGIC          <td style='text-align: left;'><b>Description: </b></td>
+# MAGIC          <td>Notebook to transfer a set of HTML, JSON, and A360 files, each representing data on Tribunal Decisions stored in ARIA, to Event Hubs.</td>
+# MAGIC       </tr>
+# MAGIC       <tr>
+# MAGIC          <td style='text-align: left;'><b>First Created: </b></td>
+# MAGIC          <td>Oct-2024</td>
+# MAGIC       </tr>
+# MAGIC       <tr>
+# MAGIC          <th style='text-align: left;'><b>Changelog (JIRA ref / initials / date):</b></th>
+# MAGIC          <th>Comments</th>
+# MAGIC       </tr>
+# MAGIC       <tr>
+# MAGIC          <td style='text-align: left;'><a href="https://tools.hmcts.net/jira/browse/ARIADM-xxx">ARIADM-xxx</a> / NSA / OCT-2024</td>
+# MAGIC          <td>Tribunal Decision and IRIS: Transfer data from Gold ADLS to Event Hubs</td>
+# MAGIC       </tr>
+# MAGIC    </tbody>
+# MAGIC </table>

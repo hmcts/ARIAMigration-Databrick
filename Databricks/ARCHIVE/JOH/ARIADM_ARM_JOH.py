a89f9d82b817a87b4679477a4360508c6c154a9f
derin-aya <derinaya101@gmail.com>
2025-01-20 13:54:25 +0000

    Parameterise bundle deployment

--- Databricks/ARCHIVE/JOH/ARIADM_ARM_JOH.py
+++ Databricks/ARCHIVE/JOH/ARIADM_ARM_JOH.py
@@ -922,22 +922,48 @@ def silver_archive_metadata():
 
 # COMMAND ----------
 
-secret = dbutils.secrets.get("ingest00-keyvault-sbox", "ingest00-adls-ingest00curatedsbox-connection-string-sbox")
+# secret = dbutils.secrets.get("ingest00-keyvault-sbox", "ingest00-adls-ingest00curatedsbox-connection-string-sbox")
 
 # COMMAND ----------
 
 # DBTITLE 1,Azure Blob Storage Connection Setup in Python
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-import os
+# from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+# import os
 
 # Set up the BlobServiceClient with your connection string
-connection_string = f"BlobEndpoint=https://ingest00curatedsbox.blob.core.windows.net/;QueueEndpoint=https://ingest00curatedsbox.queue.core.windows.net/;FileEndpoint=https://ingest00curatedsbox.file.core.windows.net/;TableEndpoint=https://ingest00curatedsbox.table.core.windows.net/;SharedAccessSignature={secret}"
+# connection_string = f"BlobEndpoint=https://ingest00curatedsbox.blob.core.windows.net/;QueueEndpoint=https://ingest00curatedsbox.queue.core.windows.net/;FileEndpoint=https://ingest00curatedsbox.file.core.windows.net/;TableEndpoint=https://ingest00curatedsbox.table.core.windows.net/;SharedAccessSignature={secret}"
 
+# blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+# Specify the container name
+# container_name = "gold"
+# container_client = blob_service_client.get_container_client(container_name)
+
+from azure.storage.blob import BlobServiceClient
+import os
+
+# Retrieve the parameters passed to the notebook
+dbutils.widgets.text("endpoint", "")
+dbutils.widgets.text("keyvault", "")
+dbutils.widgets.text("secret_name", "")
+
+endpoint = dbutils.widgets.get("endpoint")
+keyvault = dbutils.widgets.get("keyvault")
+secret_name = dbutils.widgets.get("secret_name")
+
+# Fetch the secret dynamically based on the environment
+secret = dbutils.secrets.get(scope=keyvault, key=secret_name)
+connection_string = f"{secret}"
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 # Specify the container name
 container_name = "gold"
 container_client = blob_service_client.get_container_client(container_name)
+
+# debug
+print("Listing blobs in container:")
+for blob in container_client.list_blobs():
+    print(blob.name)
 
 
 # COMMAND ----------

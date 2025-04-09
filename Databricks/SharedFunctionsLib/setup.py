1acde6ec593d198f4a5ea785939c9d1e5403d5e8
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-04-09 12:49:02 +0100

    testing python wheel and requirements.txt deployment to databricks cluster

--- Databricks/SharedFunctionsLib/setup.py
+++ Databricks/SharedFunctionsLib/setup.py
@@ -1,11 +1,19 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    requirements = f.read()
+
 setup(
-    name='ARIAFUNCITONS',
+    name='ARIAFUNCTIONS',
     version='0.0.1',
     packages=find_packages(),
     description='These are custom functions that will be used for the ARIA data migration',
     author='Ara Islam + Naveen Sriram',
+    entry_points={
+        "packages": [
+            "main=wheel_package.main:main"
+        ]
+    },
     install_requires=[]
 )
 

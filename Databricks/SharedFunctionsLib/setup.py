ab2c39ab294b0888736497c0680135eae06da8e0
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-04-14 12:36:03 +0100

    adding spark.config for tenancy in main.py, building wheel inside ADO script

--- Databricks/SharedFunctionsLib/setup.py
+++ Databricks/SharedFunctionsLib/setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='shared_functions',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     description='These are custom functions that will be used for the ARIA data migration',
-    author='Ara Islam + Naveen Sriram',
+    author='Ara Islam + Naveen Sriram + Andrew McDevitt + Graham Burnside',
     install_requires = ['azure-eventhub',
                         'azure-storage-blob',
                         'confluent_kafka',
--- Databricks/SharedFunctionsLib/shared_functions/main.py
+++ Databricks/SharedFunctionsLib/shared_functions/main.py
@@ -1,2 +1,12 @@
+import argparse
+from pyspark.sql import SparkSession
 def main():
-    print("Hello from shared_functions.main!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tenant-id")
+    args = parser.parse_args()
+    tenant_id = args.tenant_id
+    spark = SparkSession.builder.getOrCreate()
+    spark.conf.set(
+        "fs.azure.account.oauth2.client.endpoint.ingest02rawsbox.dfs.core.windows.net",
+        f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"
+    )

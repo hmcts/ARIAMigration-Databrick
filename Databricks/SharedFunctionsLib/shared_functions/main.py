61321d682e56dd422b0b0a5d3f01bb2c7283c6fd
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-04-14 13:06:17 +0100

    adding in spark_env_vars, exporting variables. re-test the spark config using the spark_env_Vars'

--- Databricks/SharedFunctionsLib/shared_functions/main.py
+++ Databricks/SharedFunctionsLib/shared_functions/main.py
@@ -1,12 +1,2 @@
-import argparse
-from pyspark.sql import SparkSession
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--tenant-id")
-    args = parser.parse_args()
-    tenant_id = args.tenant_id
-    spark = SparkSession.builder.getOrCreate()
-    spark.conf.set(
-        "fs.azure.account.oauth2.client.endpoint.ingest02rawsbox.dfs.core.windows.net",
-        f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"
-    )
+    print("Hello from shared_functions.main!")

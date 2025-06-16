e48c93bb43c80cdd97a86ebf20a2a4f9e209defb
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-06-16 17:13:05 +0100

    Fixing subdir in Azurefunction

--- AzureFunctions/ARCHIVE/td/function_app.py
+++ AzureFunctions/ARCHIVE/td/function_app.py
@@ -68,7 +68,7 @@ async def eventhub_trigger_bails(azeventhub: List[func.EventHubEvent]):
         #     container_secret = full_secret.lstrip('?')  # fallback
         container_url = f"{account_url}/{container_name}?{container_secret}"
  
-        sub_dir = f"ARIA{ARM_SEGMENT}DEV/submission" if env == "sbox" else f"ARIA{ARM_SEGMENT}/submission"
+        sub_dir = f"ARIA{ARM_SEGMENT}/submission"
         # if os.environ["ENVIRONMENT"] == "sbox":
         #     sub_dir = "ARIABDEV/submission"
         # else:

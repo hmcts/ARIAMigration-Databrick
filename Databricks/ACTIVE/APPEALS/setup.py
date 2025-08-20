29e34f8155490f09ebdb0bd533abc86f962230d3
NaveenSriramAddagarla <NaveenSriramAddagarla>
2025-08-20 11:51:20 +0000

    updates for testing

--- Databricks/ACTIVE/APPEALS/setup.py
+++ Databricks/ACTIVE/APPEALS/setup.py
@@ -1,2 +1,5 @@
 from setuptools import setup, find_packages
-setup(packages=find_packages(include = ['active_common_utils'], install_requires=['pyspark.sql.functions']))
+# setup(packages=find_packages(include = ['active_common_utils'], install_requires=['pyspark.sql.functions']))
+setup(
+    name = 'active_shared_functions', version = '0.1', py_modules=['active_common_utils']
+)

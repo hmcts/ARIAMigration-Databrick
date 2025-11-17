16c1e9aa2529b03ba07e8e74e4090d25d93d8396
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-11-17 18:06:24 +0000

    fix token path

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,8 @@ from unittest.mock import patch
 
 @pytest.fixture
 def mock_token_managers():
-    with patch("AzureFunctions.Active.active_ccd.processCase.tokenManager.IDAMTokenManager") as mock_idam, \
-         patch("AzureFunctions.Active.active_ccd.processCase.tokenManager.S2S_Manager") as mock_s2s:
+    with patch("AzureFunctions.Active.active_ccd.tokenManager.IDAMTokenManager") as mock_idam, \
+         patch("AzureFunctions.Active.active_ccd.tokenManager.S2S_Manager") as mock_s2s:
         
         # Provide mock token values to avoid errors inside process_case
         mock_idam.return_value.get_token.return_value = "mock-idam-token"

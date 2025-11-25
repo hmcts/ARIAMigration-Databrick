3b6ccef38093a4a46baa4d17bcbf96d69c4ba6e5
AndrewHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-11-25 18:51:19 +0000

    deduplicate silver_m2 in the meta data table

--- Databricks/ARCHIVE/SBAIL/Dynamic-ARIA-Scottish-Bails v2.py
+++ Databricks/ARCHIVE/SBAIL/Dynamic-ARIA-Scottish-Bails v2.py
@@ -1787,6 +1787,10 @@ def silver_m2():
 
 # COMMAND ----------
 
+
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## M3: silver_sbail_m3_hearing_details
 # MAGIC
@@ -2031,7 +2035,7 @@ from pyspark.sql import functions as F
 )
 def silver_meta_data():
   m1_df = dlt.read("silver_sbail_m1_case_details").alias("m1")
-  m2_df = dlt.read("silver_sbail_m2_case_appellant").alias("m2")
+  m2_df = dlt.read("silver_sbail_m2_case_appellant").filter(col("Relationship").isNull()).alias("m2")
   m7_df = dlt.read("silver_sbail_m7_status").alias("m7")
   max_statusid = m7_df.groupby(col("CaseNo")).agg(F.max(col("StatusId")))
 
@@ -2071,9 +2075,8 @@ def silver_meta_data():
                  F.when(F.col("m1.BaseBailType") == "BailLegalHold", "Yes").otherwise("No").alias("bf_007")
              )
     )
-
-  # Join the batchid mapping back onto the base DataFrame
-  final_df = base_df
+  #Join the batchid mapping back onto the base DataFrame
+  final_df = base_df.distinct()
     
 
 

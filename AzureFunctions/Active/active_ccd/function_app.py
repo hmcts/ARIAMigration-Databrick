8b6d9d4f29e0a528f77d3f01a220d8ef534aabb2
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2025-11-26 20:15:23 +0000

    issue fixed. revert code and ensure code is still correctly deployed

--- AzureFunctions/Active/active_ccd/function_app.py
+++ AzureFunctions/Active/active_ccd/function_app.py
@@ -23,7 +23,7 @@ except Exception:
 
 ENV = os.environ["ENVIRONMENT"]
 LZ_KEY = os.environ["LZ_KEY"]
-PR_NUMBER = os.getenv("PR_NUMBER", None)
+PR_NUMBER = os.environ["PR_NUMBER"]
 ARIA_NAME = "active"
 
 eventhub_name = f"evh-active-pub-{ENV}-{LZ_KEY}-uks-dlrm-01"

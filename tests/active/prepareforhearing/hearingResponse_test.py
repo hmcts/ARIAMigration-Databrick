8da6460a4c9fa610448345a29e1b639715928f15
misbahulislam2 <misbah.ul-islam@hmcts.net>
2026-01-15 17:51:18 +0000

    All tests Added

--- tests/active/prepareforhearing/hearingResponse_test.py
+++ tests/active/prepareforhearing/hearingResponse_test.py
@@ -31,11 +31,11 @@ def hearingResponse_outputs(spark):
     ])
 
     m1_data = [
-        ("CASE001", "AIP", "FTPA", None, 0, 0, True, 1),  # LanguageCode 1 - Spoken Language
-        ("CASE002", "AIP", "FTPA", None, 0, 0, False, 2),  # LanguageCode 5 - Spoken Language Manual Entry
+        ("CASE001", "AIP", "FTPA", None, 0, 1, True, 1),  # LanguageCode 1 - Spoken Language
+        ("CASE002", "AIP", "FTPA", None, 0, 2, False, 2),  # LanguageCode 5 - Spoken Language Manual Entry
         ("CASE003", "AIP", "FTPA", None, 0, 0, True, 2),  # LanguageCode 6 - Sign Language
-        ("CASE004", "AIP", "FTPA", None, 0, 0, False, 2),  # LanguageCode 7 - Sign Language Manual Entry
-        ("CASE005", "AIP", "FT", None, 0, 0, True, 3), 
+        ("CASE004", "AIP", "FTPA", None, 0, None, False, 2),  # LanguageCode 7 - Sign Language Manual Entry
+        ("CASE005", "AIP", "FT", None, 0, 33, True, 3), 
         ("CASE006", "AIP", "FT", None, 0, 0, True, 4),    # For m3 conditional tests - Additional Language Spoken + Spoken Manual
         ("CASE007", "AIP", "FT", None, 0, 0, False, None),    # For m3 conditional tests - Additional Language Spoken + Sign
         ("CASE008", "AIP", "FT", None, 0, 0, False, None),    # For m3 conditional tests - Additional Language Spoken + Sign Manual
@@ -168,3 +168,153 @@ def test_multimediaDecisionForDisplay(spark,hearingResponse_outputs):
     assert results["CASE001"]["multimediaDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
     assert results["CASE002"]["multimediaDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
     assert results["CASE006"]["multimediaDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+
+def test_isInCameraCourtAllowed(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["isInCameraCourtAllowed"] == 'Granted'
+    assert results["CASE002"]["isInCameraCourtAllowed"] == None
+    assert results["CASE009"]["isInCameraCourtAllowed"] == None
+
+def test_inCameraCourtTribunalResponse(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["inCameraCourtTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["inCameraCourtTribunalResponse"] == None
+    assert results["CASE009"]["inCameraCourtTribunalResponse"] == None
+
+def test_inCameraCourtDecisionForDisplay(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["inCameraCourtDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["inCameraCourtDecisionForDisplay"] == None
+    assert results["CASE009"]["inCameraCourtDecisionForDisplay"] == None
+
+def test_isSingleSexCourtAllowed(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["isSingleSexCourtAllowed"] == 'Granted'
+    assert results["CASE002"]["isSingleSexCourtAllowed"] == 'Granted'
+    assert results["CASE003"]["isSingleSexCourtAllowed"] == None
+    assert results["CASE004"]["isSingleSexCourtAllowed"] == None
+
+def test_singleSexCourtTribunalResponse(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["singleSexCourtTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["singleSexCourtTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE003"]["singleSexCourtTribunalResponse"] == None
+    assert results["CASE004"]["singleSexCourtTribunalResponse"] == None
+
+def test_singleSexCourtDecisionForDisplay(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["singleSexCourtDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["singleSexCourtDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE003"]["singleSexCourtDecisionForDisplay"] == None
+    assert results["CASE004"]["singleSexCourtDecisionForDisplay"] == None
+
+
+
+
+def test_isVulnerabilitiesAllowed(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["isVulnerabilitiesAllowed"] == 'Granted'
+    assert results["CASE002"]["isVulnerabilitiesAllowed"] == 'Granted'
+    assert results["CASE008"]["isVulnerabilitiesAllowed"] == 'Granted'
+
+def test_vulnerabilitiesTribunalResponse(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["vulnerabilitiesTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["vulnerabilitiesTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE008"]["vulnerabilitiesTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+
+def test_vulnerabilitiesDecisionForDisplay(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["vulnerabilitiesDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["vulnerabilitiesDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE008"]["vulnerabilitiesDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+
+
+def test_isRemoteHearingAllowed(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["isRemoteHearingAllowed"] == 'Granted'
+    assert results["CASE002"]["isRemoteHearingAllowed"] == 'Granted'
+    assert results["CASE008"]["isRemoteHearingAllowed"] == 'Granted'
+
+def test_remoteVideoCallTribunalResponse(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["remoteVideoCallTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["remoteVideoCallTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE008"]["remoteVideoCallTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+
+def test_remoteHearingDecisionForDisplay(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["remoteHearingDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["remoteHearingDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE008"]["remoteHearingDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+
+def test_isAdditionalAdjustmentsAllowed(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["isAdditionalAdjustmentsAllowed"] == 'Granted'
+    assert results["CASE002"]["isAdditionalAdjustmentsAllowed"] == 'Granted'
+    assert results["CASE008"]["isAdditionalAdjustmentsAllowed"] == 'Granted'
+
+def test_additionalTribunalResponse(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["additionalTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["additionalTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE008"]["additionalTribunalResponse"] == 'This is a migrated ARIA case. Please refer to the documents.'
+
+def test_otherDecisionForDisplay(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["otherDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE002"]["otherDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+    assert results["CASE008"]["otherDecisionForDisplay"] == 'Granted - This is a migrated ARIA case. Please refer to the documents.'
+
+def test_isAdditionalInstructionAllowed(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["isAdditionalInstructionAllowed"] == 'Yes'
+    assert results["CASE002"]["isAdditionalInstructionAllowed"] == 'Yes'
+    assert results["CASE008"]["isAdditionalInstructionAllowed"] == 'Yes'
+
+def test_additionalInstructionsTribunalResponse(spark,hearingResponse_outputs):
+
+    results = hearingResponse_outputs
+
+    assert results["CASE001"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC001\n Hearing Date: 2023-10-01\n Hearing Type: HearingType\n Court: CourtName\n List Type: Standard\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name Jud_F1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: CC_Sur CC_Fore (CC_T)\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: 180\n Required/Incompatible Judicial Officers: Jud_S_Name Jud_F_Name ( Judge_T ) : Required\n Notes: xxxx'
+    assert results["CASE002"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC002\n Hearing Date: 2023-10-01\n Hearing Type: N/A\n Court: CourtName\n List Type: Urgent\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name Jud_F1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: N/A\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: 60\n Required/Incompatible Judicial Officers: Jud_F_Name ( Judge_T ) : Not Required\n Notes: Noxxxxtes'
+    assert results["CASE003"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC003\n Hearing Date: 2023-10-01\n Hearing Type: HearingType\n Court: CourtName\n List Type: Special\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name Jud_F1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: CC_Sur (CC_T)\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: 240\n Required/Incompatible Judicial Officers: Jud_S_Name ( Judge_T ) : Required\n Notes: Notes'
+    assert results["CASE004"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC004\n Hearing Date: 2023-10-01\n Hearing Type: N/A\n Court: CourtName\n List Type: Standard\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: CC_Sur CC_Fore\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: 360\n Required/Incompatible Judicial Officers: Jud_S_Name Jud_F_Name : Required\n Notes: Notes'
+    assert results["CASE005"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC005\n Hearing Date: 2023-10-01\n Hearing Type: HearingType\n Court: N/A\n List Type: Urgent\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name Jud_F1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: N/A\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: N/A\n Required/Incompatible Judicial Officers: ( Judge_T ) : Not Required\n Notes: N/A'
+    assert results["CASE006"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC006\n Hearing Date: 2023-10-01\n Hearing Type: HearingType\n Court: CourtName\n List Type: Special\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name Jud_F1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: N/A\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: 30\n Required/Incompatible Judicial Officers: : Not Required\n Notes: Notes'
+    assert results["CASE007"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC007\n Hearing Date: 2023-10-01\n Hearing Type: HearingType\n Court: CourtName\n List Type: Standard\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name Jud_F1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: CC_Sur CC_Fore (CC_T)\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: N/A\n Required/Incompatible Judicial Officers: Jud_S_Name Jud_F_Name ( Judge_T )\n Notes: Notes'
+    assert results["CASE008"]["additionalInstructionsTribunalResponse"] == 'Listed details from ARIA: \n Hearing Centre: LOC008\n Hearing Date: 2023-10-01\n Hearing Type: HearingType\n Court: CourtName\n List Type: Urgent\n List Start Time: 2023-10-01T10:00:00\n Judge First Tier: Jud_S1_Name Jud_F1_Name (Judge1_T) Jud_S2_Name Jud_F2_Name (Judge2_T) Jud_S3_Name Jud_F3_Name (Judge3_T)\n Court Clerk / Usher: CC_Sur CC_Fore (CC_T)\n Start Time: 2023-10-01T10:00:00\n Estimated Duration: 45\n Required/Incompatible Judicial Officers: Jud_S_Name Jud_F_Name ( Judge_T )\n Notes: Notes'
+
+

5c1b7365378de0c15b821cd827713138ad40c036
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-01-26 09:17:46 +0000

    renamed function to partyID matching func in live code

--- tests/active/paymentPending/PartyID_test.py
+++ tests/active/paymentPending/PartyID_test.py
@@ -1,6 +1,6 @@
 import pytest
 from pyspark.sql import SparkSession, types as T
-from Databricks.ACTIVE.APPEALS.shared_functions.paymentPending import PartyID
+from Databricks.ACTIVE.APPEALS.shared_functions.paymentPending import partyID
 import uuid
 
 @pytest.fixture(scope="session")
@@ -12,7 +12,7 @@ def spark():
     )
 
 @pytest.fixture(scope="session")
-def PartyID_outputs(spark):
+def partyID_outputs(spark):
 
     m1_schema = T.StructType([
         T.StructField("CaseNo", T.StringType(), True),
@@ -68,10 +68,10 @@ def PartyID_outputs(spark):
     silver_c =  spark.createDataFrame(silver_c_data, silver_c_schema)
 
     # Call the function under test
-    PartyID_content, _ = PartyID(silver_m1, silver_m3, silver_c)
+    partyID_content, _ = partyID(silver_m1, silver_m3, silver_c)
 
     # Convert to dictionary keyed by CaseNo
-    results = {row["CaseNo"]: row.asDict() for row in PartyID_content.collect()}
+    results = {row["CaseNo"]: row.asDict() for row in partyID_content.collect()}
     return results
 
 def is_uuid(v):
@@ -81,30 +81,30 @@ def is_uuid(v):
     except Exception:
         return False
 
-def test_appellant_party_id_created(PartyID_outputs):
-    row = PartyID_outputs["HU/00638/2024"]
+def test_appellant_party_id_created(partyID_outputs):
+    row = partyID_outputs["HU/00638/2024"]
     assert is_uuid(row["appellantPartyId"])
 
-def test_legal_rep_created_for_LR(PartyID_outputs):
-    row = PartyID_outputs["HU/00638/2024"]
+def test_legal_rep_created_for_LR(partyID_outputs):
+    row = partyID_outputs["HU/00638/2024"]
     assert is_uuid(row["legalRepIndividualPartyId"])
     assert is_uuid(row["legalRepOrganisationPartyId"])
 
-def test_legal_rep_null_for_AIP(PartyID_outputs):
-    row = PartyID_outputs["EA/00556/2025"]
+def test_legal_rep_null_for_AIP(partyID_outputs):
+    row = partyID_outputs["EA/00556/2025"]
     assert row["legalRepIndividualPartyId"] is None
     assert row["legalRepOrganisationPartyId"] is None
 
-def test_sponsor_created_when_category_38_and_name(PartyID_outputs):
-    row = PartyID_outputs["HU/00638/2024"]
+def test_sponsor_created_when_category_38_and_name(partyID_outputs):
+    row = partyID_outputs["HU/00638/2024"]
     assert is_uuid(row["sponsorPartyId"])
 
-def test_sponsor_null_without_name(PartyID_outputs):
-    row = PartyID_outputs["HU/01871/2024"]
+def test_sponsor_null_without_name(partyID_outputs):
+    row = partyID_outputs["HU/01871/2024"]
     assert row["sponsorPartyId"] is None
 
-def test_no_ids_when_appeal_type_null(PartyID_outputs):
-    row = PartyID_outputs["HU/00592/2025"]
+def test_no_ids_when_appeal_type_null(partyID_outputs):
+    row = partyID_outputs["HU/00592/2025"]
     assert row["appellantPartyId"] is None
     assert row["legalRepIndividualPartyId"] is None
     assert row["legalRepOrganisationPartyId"] is None

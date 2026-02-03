88a49469bfad7038bde72a08106f0ebbbb65917b
misbahulislam2 <misbah.ul-islam@hmcts.net>
2026-02-03 17:09:35 +0000

    Added all unit tests

--- tests/active/ftpa_submitted_a/ftpa_test.py
+++ tests/active/ftpa_submitted_a/ftpa_test.py
@@ -1,4 +1,4 @@
-from Databricks.ACTIVE.APPEALS.shared_functions.decided_a import ftpa
+from Databricks.ACTIVE.APPEALS.shared_functions.ftpa_submitted_a import ftpa
 from pyspark.sql import SparkSession
 import pytest
 
@@ -18,49 +18,6 @@ def spark():
 @pytest.fixture(scope="session")
 def ftpa_outputs(spark):
 
-    m1_schema = T.StructType([
-    T.StructField("CaseNo", T.StringType(), True),
-    T.StructField("dv_representation", T.StringType(), True),
-    T.StructField("lu_appealType", T.StringType(), True),
-    T.StructField("Sponsor_Name", T.StringType(), True),
-    T.StructField("Interpreter", T.StringType(), True),
-    T.StructField("CourtPreference", T.StringType(), True),
-    T.StructField("InCamera", T.BooleanType(), True),
-    T.StructField("VisitVisaType", T.IntegerType(), True),
-    T.StructField("CentreId", T.IntegerType(), True),
-    T.StructField("Rep_Postcode", T.StringType(), True),
-    T.StructField("CaseRep_Postcode", T.StringType(), True),
-    T.StructField("PaymentRemissionRequested", T.IntegerType(), True),
-    T.StructField("lu_applicationChangeDesignatedHearingCentre", T.StringType(), True),
-    ])
-
-    m1_data = [
-        ("CASE001", "AIP", "FTPA", None, 0, 0, True, 1,1, "B12 0hf", "B12 0hf",1,"Man"),  
-        ("CASE002", "AIP", "FTPA", None, 0, 0, False, 2,2, "B12 0hf", "B12 0hf",2,"Man"),  
-        ("CASE003", "AIP", "FTPA", None, 0, 0, True, 2,3, "B12 0hf", "B12 0hf",3,"Man"),  
-        ("CASE004", "AIP", "FTPA", None, 0, 0, False, 2,4, "B12 0hf", "B12 0hf",1,"Man"),  
-        ("CASE005", "AIP", "FT", None, 0, 0, True, 3,5, "B12 0hf", "B12 0hf",2,"Man"), 
-        ("CASE006", "AIP", "FT", None, 0, 0, True, 4,6, "B12 0hf", "B12 0hf",4,"Man"),    
-        ("CASE007", "AIP", "FT", None, 0, 0, False, None,7, "B12 0hf", "B12 0hf",0,"Man"),    
-        ("CASE008", "AIP", "FT", None, 0, 0, False, None,8, "B12 0hf", "B12 0hf",None,"Man"),    
-        ("CASE009", "AIP", "FT", None, 0, 0, None, 2,9, "B12 0hf", "B12 0hf",7,"Man"),    
-        ("CASE010", "AIP", "FT", None, 0, 0, None, 2,None, "B12 0hf", "B12 0hf",1,"Man"),  
-        ("CASE011", "AIP", "FT", None, 0, 0, True, 61,0, "B12 0hf", "B12 0hf",1,"Man")
-        ]
-    
-    m2_schema = T.StructType([
-    T.StructField("CaseNo", T.StringType(), True),
-    T.StructField("Appellant_Name", T.StringType(), True),
-    T.StructField("Appellant_Postcode", T.StringType(), True),
-    T.StructField("Relationship", T.StringType(), True)])
-    
-    m2_data = [
-        ("CASE001", "Gold", "B12 0hf","Relationship1"),  
-        ("CASE002", "Smith", "M8 1XY","Relationship2"),  
-        ("CASE003", "Johns", "DE4 9HN","Relationship3"),  
-        ("CASE004", "Black", "BN6 0PA","Relationship4"),  
-        ("CASE005", "Green", "DD7 7PT",None), 
-        ]
 
     m3_schema = T.StructType([
         T.StructField("CaseNo", T.StringType(), True),
@@ -68,26 +25,34 @@ def ftpa_outputs(spark):
         T.StructField("CaseStatus", T.IntegerType(), True),
         T.StructField("HearingDuration", T.IntegerType(), True),
         T.StructField("HearingCentre", T.StringType(), True),
+        T.StructField("DateReceived", T.StringType(), True),
         T.StructField("DecisionDate", T.StringType(), True),
-        T.StructField("StartTime", T.StringType(), True),
-        T.StructField("Adj_Determination_Title", T.StringType(), True),
-        T.StructField("Adj_Determination_Forenames", T.StringType(), True),
-        T.StructField("Adj_Determination_Surname", T.StringType(), True),
+        T.StructField("Adj_Title", T.StringType(), True),
+        T.StructField("Adj_Forenames", T.StringType(), True),
+        T.StructField("Adj_Surname", T.StringType(), True),
+        T.StructField("Party", T.IntegerType(), True),
+        T.StructField("OutOfTime", T.BooleanType(), True),
         T.StructField("Outcome", T.IntegerType(), True),
     ])
 
     m3_data = [
-        ("CASE005", 1, 37, 180, "LOC001","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Mr","Doe","John",1),
-        ("CASE005", 2, 37, 60, "LOC002","2025-11-02T00:00:00.000+00:00","1899-12-30T12:00:00.000+00:00","Ms","Doe","Jane",1),   
-        ("CASE006", 1, 38, 240, "LOC003","2026-12-03T00:00:00.000+00:00","1899-12-30T13:00:00.000+00:00","Mr","xyz","John",2),   
-        ("CASE007", 1, 38, 360, "LOC004","2026-08-03T00:00:00.000+00:00","2000-12-30T07:10:58.000+00:00","Mr","Doe","abc",2),  
-        ("CASE008", 1, 37, None, "LOC005","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Sir","Random","Guy",None),  
-        ("CASE009", 1, 37, 30, "LOC006","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Mr","John","Snow",3),  
-        ("CASE010", 1, 38, None, "LOC007",None,"1899-12-30T10:00:00.000+00:00",None,None,None,1),  
-        ("CASE011", 1, 38, 45, "LOC008","2025-11-02T00:00:00.000+00:00","1899-12-30T12:00:00.999+00:00","Mr","Hello","World",2)   
-        ]      
-
-    
+        ("CASE005", 1, 37, 180, "LOC001","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Mr","Doe","John",1,True,1),
+        ("CASE005", 2, 38, 60, "LOC002","2025-11-02T00:00:00.000+00:00","1899-12-30T12:00:00.000+00:00","Ms","Doe","Jane",1,False,1),   
+        ("CASE006", 1, 26, 240, "LOC003","2026-12-03T00:00:00.000+00:00","1899-12-30T13:00:00.000+00:00","Mr","xyz","John",1,True,1),   
+        ("CASE007", 1, 37, 360, "LOC004","2026-08-03T00:00:00.000+00:00","2000-12-30T07:10:58.000+00:00","Mr","Doe","abc",2,False,1),  
+        ("CASE008", 1, 38, None, "LOC005","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Sir","Random","Guy",1,True,1),  
+        ("CASE009", 1, 26, 30, "LOC006","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Mr","John","Snow",2,False,1),  
+        ("CASE010", 1, 37, None, "LOC007",None,"1899-12-30T10:00:00.000+00:00",None,None,None,1,None,1),  
+        ("CASE011", 1, 38, 45, "LOC008","2025-11-02T00:00:00.000+00:00","1899-12-30T12:00:00.999+00:00","Mr","Hello","World",2,True,1),   
+        ("CASE005", 1, 39, 180, "LOC001","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Mr","Doe","John",1,True,1),
+        ("CASE005", 2, 39, 60, "LOC002","2025-11-02T00:00:00.000+00:00","1899-12-30T12:00:00.000+00:00","Ms","Doe","Jane",1,False,1),   
+        ("CASE006", 1, 39, 240, "LOC003","2026-12-03T00:00:00.000+00:00","1899-12-30T13:00:00.000+00:00","Mr","xyz","John",1,True,1),   
+        ("CASE007", 1, 39, 360, "LOC004","2026-08-03T00:00:00.000+00:00","2000-12-30T07:10:58.000+00:00","Mr","Doe","abc",2,False,1),  
+        ("CASE008", 1, 39, None, "LOC005","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Sir","Random","Guy",1,True,1),  
+        ("CASE009", 1, 39, 30, "LOC006","2024-10-02T00:00:00.000+00:00","1899-12-30T10:00:00.000+00:00","Mr","John","Snow",2,False,1),  
+        ("CASE010", 1, 39, None, "LOC007",None,"1899-12-30T10:00:00.000+00:00",None,None,None,1,None,1),  
+        ("CASE011", 1, 39, 45, "LOC008","2025-11-02T00:00:00.000+00:00","1899-12-30T12:00:00.999+00:00","Mr","Hello","World",2,True,1)
+        ]     
 
     c_schema = T.StructType([
     T.StructField("CaseNo", T.StringType(), True),
@@ -96,15 +61,14 @@ def ftpa_outputs(spark):
     
     c_data = [
         ("CASE005", 37),  
-        ("CASE006", 38),  
-        ("CASE007", None),  
-        ("CASE008", None),  
-        ("CASE009", None), 
-        ("CASE010", None),
+        ("CASE006", 37),  
+        ("CASE007", 37),  
+        ("CASE008", 37),  
+        ("CASE009", 37), 
+        ("CASE010", 37),
+        ("CASE011", 37),
         ]
     
-    df_m1 =  spark.createDataFrame(m1_data, m1_schema)
-    df_m2 =  spark.createDataFrame(m2_data, m2_schema)
     df_m3 =  spark.createDataFrame(m3_data, m3_schema)
     df_c =  spark.createDataFrame(c_data, c_schema)
 
@@ -114,13 +78,68 @@ def ftpa_outputs(spark):
     
     return results
 
-def test_ftpaApplicationDeadline(spark,ftpa_outputs):
+def test_ftpaList(spark,ftpa_outputs):
 
     results = ftpa_outputs
 
-    assert results["CASE005"]["ftpaApplicationDeadline"] == "16/11/2025"
-    assert results["CASE006"]["ftpaApplicationDeadline"] == "31/12/2026"
-    assert results["CASE007"]["ftpaApplicationDeadline"] == '03/08/2026'
-    assert results["CASE010"]["ftpaApplicationDeadline"] == None
+    assert results["CASE005"]["ftpaList"] == [Row(id='1', value=Row(ftpaApplicant='appellant', ftpaApplicationDate='02/11/2025', ftpaGroundsDocuments=[], ftpaEvidenceDocuments=[], ftpaOutOfTimeDocuments=[], ftpaOutOfTimeExplanation=None))]
+    assert results["CASE006"]["ftpaList"] == [Row(id='1', value=Row(ftpaApplicant='appellant', ftpaApplicationDate='03/12/2026', ftpaGroundsDocuments=[], ftpaEvidenceDocuments=[], ftpaOutOfTimeDocuments=[], ftpaOutOfTimeExplanation='This is a migrated ARIA case. Please refer to the documents.'))]
+    assert results["CASE007"]["ftpaList"] == [Row(id='1',value=Row(ftpaApplicant='respondent',ftpaApplicationDate='03/08/2026',ftpaGroundsDocuments=[],ftpaEvidenceDocuments=[],ftpaOutOfTimeDocuments=[],ftpaOutOfTimeExplanation=None))]
+    assert results["CASE011"]["ftpaList"] == [Row(id='1', value=Row(ftpaApplicant='respondent', ftpaApplicationDate='02/11/2025', ftpaGroundsDocuments=[], ftpaEvidenceDocuments=[], ftpaOutOfTimeDocuments=[], ftpaOutOfTimeExplanation='This is a migrated ARIA case. Please refer to the documents.'))]
 
+def test_ftpaAppellantApplicationDate(spark,ftpa_outputs):
+
+    results = ftpa_outputs
+
+    assert results["CASE005"]["ftpaAppellantApplicationDate"] == "02/11/2025"
+    assert results["CASE006"]["ftpaAppellantApplicationDate"] == "03/12/2026"
+    assert results["CASE007"]["ftpaAppellantApplicationDate"] == None
+    assert results["CASE010"]["ftpaAppellantApplicationDate"] == None
+
+def test_ftpaAppellantSubmissionOutOfTime(spark,ftpa_outputs):
+
+    results = ftpa_outputs
+
+    assert results["CASE005"]["ftpaAppellantSubmissionOutOfTime"] == "No"
+    assert results["CASE006"]["ftpaAppellantSubmissionOutOfTime"] == "Yes"
+    assert results["CASE007"]["ftpaAppellantSubmissionOutOfTime"] == None
+    assert results["CASE010"]["ftpaAppellantSubmissionOutOfTime"] == None
+
+def test_ftpaAppellantOutOfTimeExplanation(spark,ftpa_outputs):
+
+    results = ftpa_outputs
+
+    assert results["CASE005"]["ftpaAppellantOutOfTimeExplanation"] == None
+    assert results["CASE006"]["ftpaAppellantOutOfTimeExplanation"] == "This is a migrated ARIA case. Please refer to the documents."
+    assert results["CASE007"]["ftpaAppellantOutOfTimeExplanation"] == None
+    assert results["CASE010"]["ftpaAppellantOutOfTimeExplanation"] == None
+
+
+def test_ftpaRespondentApplicationDate(spark,ftpa_outputs):
+
+    results = ftpa_outputs
+
+    assert results["CASE005"]["ftpaRespondentApplicationDate"] == None
+    assert results["CASE006"]["ftpaRespondentApplicationDate"] == None
+    assert results["CASE007"]["ftpaRespondentApplicationDate"] == "03/08/2026"
+    assert results["CASE011"]["ftpaRespondentApplicationDate"] == "02/11/2025"
+
+
+def test_ftpaRespondentSubmissionOutOfTime(spark,ftpa_outputs):
+
+    results = ftpa_outputs
+
+    assert results["CASE005"]["ftpaRespondentSubmissionOutOfTime"] == None
+    assert results["CASE006"]["ftpaRespondentSubmissionOutOfTime"] == None
+    assert results["CASE007"]["ftpaRespondentSubmissionOutOfTime"] == "No"
+    assert results["CASE011"]["ftpaRespondentSubmissionOutOfTime"] == "Yes"
+
+def test_ftpaRespondentOutOfTimeExplanation(spark,ftpa_outputs):
+
+    results = ftpa_outputs
+
+    assert results["CASE005"]["ftpaRespondentOutOfTimeExplanation"] == None
+    assert results["CASE006"]["ftpaRespondentOutOfTimeExplanation"] == None
+    assert results["CASE007"]["ftpaRespondentOutOfTimeExplanation"] == None
+    assert results["CASE011"]["ftpaRespondentOutOfTimeExplanation"] == "This is a migrated ARIA case. Please refer to the documents."
 

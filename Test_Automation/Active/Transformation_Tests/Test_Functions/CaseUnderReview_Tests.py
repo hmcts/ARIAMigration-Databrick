1771858b6d840c7b110efcc4022450e33b4f9ec2
rafalam1 <Rafiul.Alam@HMCTS.NET>
2026-02-25 15:49:51 +0000

    adding default mapping for caseUnderReview

--- Test_Automation/Active/Transformation_Tests/Test_Functions/CaseUnderReview_Tests.py
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/CaseUnderReview_Tests.py
@@ -20,3 +20,59 @@ def testcase2():
 
 
 
+############################################################################################
+#######################
+#default mapping Init code
+#######################
+def test_default_mapping_init(json, M1_silver):
+    try:
+        test_df = json.select(
+            "appealReferenceNumber",
+            "caseArgumentAvailable"
+        )
+
+        M1_silver = M1_silver.select(
+            "CaseNo",
+            "dv_representation"
+        )
+
+        test_df = test_df.join(
+            M1_silver,
+            json["appealReferenceNumber"] == M1_silver["CaseNo"],
+            "inner"
+        ).drop(M1_silver["CaseNo"])
+
+        return test_df, True
+    except Exception as e:
+        error_message = str(e)        
+        return None,TestResult("DefaultMapping", "FAIL",f"Failed to Setup Data for Test : Error : {error_message[:300]}",test_from_state,inspect.stack()[0].function)
+
+def test_defaultValues(test_df):
+    try:
+        results_list = []
+
+        acceptance_critera_lr = test_df.filter(
+            ((col("dv_representation") == "LR") & (col("caseArgumentAvailable") != "Yes"))
+        )
+
+        if acceptance_critera_lr.count() != 0:
+            results_list.append(TestResult(
+                "caseArgumentAvailable", 
+                "FAIL", 
+                f"Failed to check Default Mapping for : caseArgumentAvailable - expected : 'Yes' - found {acceptance_critera_lr.count()} records not matching", 
+                test_from_state,
+                inspect.stack()[0].function
+            ))
+        else:
+            results_list.append(TestResult(
+                "caseArgumentAvailable", 
+                "PASS", 
+                f"Checked Default Mapping for : caseArgumentAvailable - found correct value", 
+                test_from_state,
+                inspect.stack()[0].function
+            ))
+            
+        return results_list
+    except Exception as e:
+        error_message = str(e)        
+        return [TestResult("DefaultMapping", "FAIL",f"TEST FAILED WITH EXCEPTION :  Error : {error_message[:300]}", test_from_state, inspect.stack()[0].function)]

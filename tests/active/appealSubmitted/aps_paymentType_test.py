1c5a3d4cf72231465a887af50335f5ae37dfbae2
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-02-26 11:53:05 +0000

    test

--- tests/active/appealSubmitted/aps_paymentType_test.py
+++ tests/active/appealSubmitted/aps_paymentType_test.py
@@ -254,11 +254,11 @@ class TestAppealSubmittedPaymentType:
             # assert resultList[2][0] == "100" 
             assert resultList[3][0] == "0" 
             assert resultList[4][0] is None
-            # assert resultList[5][0] is None
+            assert resultList[5][0] == "0"
             assert resultList[6][0] == "0"
             assert resultList[7][0] == "0" 
             assert resultList[8][0] == "0"
-            assert resultList[9][0] == "500"
+            # assert resultList[9][0] == "500"
 
     def test_additionalPaymentInfo(self, spark):
         with patch('Databricks.ACTIVE.APPEALS.shared_functions.appealSubmitted.PP') as PP:

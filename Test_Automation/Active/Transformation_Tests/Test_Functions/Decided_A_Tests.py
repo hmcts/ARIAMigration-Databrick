f4ff7d1234448be7db80261f11c1aa747cc68eba
PeteGresty <peter.gresty@hmcts.net>
2026-03-12 11:29:54 +0000

    added transformation test housing for states 8,9 and 10

---
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/Decided_A_Tests.py
@@ -0,0 +1,22 @@
+from pyspark.sql.functions import (
+    col, when, lit, array, struct, collect_list, 
+    max as spark_max, date_format, row_number, expr, 
+    size, udf, coalesce, concat_ws, concat, trim, year, split, datediff,
+    collect_set, current_timestamp,transform, first, array_contains
+)
+import inspect
+
+#Import Test Results class
+from models.test_result import TestResult
+
+#Temp solution : using variable below, when each testresult instance is created, to tag with where test run from
+test_from_state = "decided(a)"
+
+def testcase1():    
+    return TestResult("testcasefield", "FAIL", f"decided(a) - test 1 complete", test_from_state, inspect.stack()[0].function)
+
+def testcase2():    
+    return TestResult("testcasefield", "PASS", f"decided(a) - test 2 complete", test_from_state, inspect.stack()[0].function)
+
+
+

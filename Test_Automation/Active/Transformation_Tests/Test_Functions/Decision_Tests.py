14dc2853f31f12449d51b65b5234dbf52b7121b2
rafalam1 <Rafiul.Alam@HMCTS.NET>
2026-03-13 15:36:02 +0000

    decision default mapping test

--- Test_Automation/Active/Transformation_Tests/Test_Functions/Decision_Tests.py
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/Decision_Tests.py
@@ -12,11 +12,98 @@ from models.test_result import TestResult
 #Temp solution : using variable below, when each testresult instance is created, to tag with where test run from
 test_from_state = "decision"
 
-def testcase1():    
-    return TestResult("testcasefield", "FAIL", f"decision - test 1 complete", test_from_state, inspect.stack()[0].function)
+############################################################################################
+#######################
+#default mapping Init code
+#######################
+def test_default_mapping_init(json):
+    try:
+        test_df = json.select(
+            "scheduleOfIssuesAgreement",
+            "scheduleOfIssuesDisagreementDescription",
+            "immigrationHistoryAgreement",
+            "immigrationHistoryDisagreementDescription",
+            "hmcts",
+            "stitchingStatus",
+            "bundleConfiguration",
+            "decisionAndReasonsAvailable",
+            "caseBundles"
+        )
+        return test_df, True
+    except Exception as e:
+        error_message = str(e)        
+        return None,TestResult("DefaultMapping", "FAIL",f"Failed to Setup Data for Test : Error : {error_message[:300]}", test_from_state, inspect.stack()[0].function)
 
-def testcase2():    
-    return TestResult("testcasefield", "PASS", f"decision - test 2 complete", test_from_state, inspect.stack()[0].function)
+def test_dec_defaultValues(test_df, fields_to_exclude):
+    try:
+        expected_defaults = {
+            "scheduleOfIssuesAgreement": "No",
+            "scheduleOfIssuesDisagreementDescription": "This is a migrated ARIA case. Please see the documents for information on the schedule of issues.",
+            "immigrationHistoryAgreement": "No",
+            "immigrationHistoryDisagreementDescription": "This is a migrated ARIA case. Please see the documents for information on the immigration history.",
+            "stitchingStatus": "DONE",
+            "bundleConfiguration": "iac-hearing-bundle-config.yaml",
+            "decisionAndReasonsAvailable": "No"
+
+        }
+
+        expected_arrays = {
+        #    "hmcts":[userImage:hmcts.png],
+           "caseBundles": None
+        }
+        
+        results_list = []
+
+        for field, expected in expected_defaults.items():
+            if field in fields_to_exclude:
+                continue
+            condition = (col(field) != expected)
+            if test_df.filter(condition).count() > 0:
+                results_list.append(TestResult(
+                    field, 
+                    "FAIL", 
+                    f"Failed to check Default Mapping for : {field} - expected : {expected} - found {str(test_df.filter(condition).count())} records not matching", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
+            else:
+                results_list.append(TestResult(
+                    field, 
+                    "PASS", 
+                    f"Checked Default Mapping for : {field} - found correct value : {expected}", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
+
+        for field, contains_val in expected_arrays.items():
+            if field in fields_to_exclude:
+                continue
+            if contains_val:
+                condition = (~array_contains(col(field), contains_val))
+            else:
+                condition = (size(col(field)) != 0)
+                
+            if test_df.filter(condition).count() > 0:
+                results_list.append(TestResult(
+                    field, 
+                    "FAIL", 
+                    f"Failed to check Default Mapping for : {field} - expected : {expected} - found {str(test_df.filter(condition).count())} records not matching", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
+            else:
+                results_list.append(TestResult(
+                    field, 
+                    "PASS", 
+                    f"Checked Default Mapping for : {field} - found correct value : {expected}", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
+
+        return results_list
+    except Exception as e:
+        error_message = str(e)        
+        return [TestResult("DefaultMapping", "FAIL",f"TEST FAILED WITH EXCEPTION :  Error : {error_message[:300]}", test_from_state, inspect.stack()[0].function)]
 
 
 

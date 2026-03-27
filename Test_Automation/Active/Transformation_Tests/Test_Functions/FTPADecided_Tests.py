6603d3f841fc1f02bc39c2d46aeea771b0bbd56d
NazeeKadiu <nazee.kadiu1@hmcts.net>
2026-03-27 14:16:09 +0000

    Add FTPADecided default mapping tests

--- Test_Automation/Active/Transformation_Tests/Test_Functions/FTPADecided_Tests.py
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/FTPADecided_Tests.py
@@ -12,11 +12,55 @@ from models.test_result import TestResult
 #Temp solution : using variable below, when each testresult instance is created, to tag with where test run from
 test_from_state = "ftpaDecided"
 
-def testcase1():    
-    return TestResult("testcasefield", "FAIL", f"ftpaDecided - test 1 complete", test_from_state, inspect.stack()[0].function)
+############################################################################################
 
-def testcase2():    
-    return TestResult("testcasefield", "PASS", f"ftpaDecided - test 2 complete", test_from_state, inspect.stack()[0].function)
+#######################
+# default mapping Init code
+#######################
+
+def test_default_mapping_init(json):
+    try:
+        test_df = json.select(
+            "isDlrmSetAsideEnabled",
+            "isReheardAppealEnabled"
+        )
+        return test_df, True
+    except Exception as e:
+        error_message = str(e)        
+        return None,TestResult("DefaultMapping", "FAIL",f"Failed to Setup Data for Test : Error : {error_message[:300]}", test_from_state, inspect.stack()[0].function)
+
+def test_ftpaDecided_defaultValues(test_df, fields_to_exclude):
+    try:
+        expected_defaults = {
+            "isDlrmSetAsideEnabled": "Yes",
+            "isReheardAppealEnabled": "Yes"
+        }
+        
+        results_list = []
+
+        for field, expected in expected_defaults.items():
+            if field in fields_to_exclude:
+                continue
+            condition = (col(field) != expected)
+            if test_df.filter(condition).count() > 0:
+                results_list.append(TestResult(
+                    field, 
+                    "FAIL", 
+                    f"Failed to check Default Mapping for : {field} - expected : {expected} - found {str(test_df.filter(condition).count())} records not matching", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
+            else:
+                results_list.append(TestResult(
+                    field, 
+                    "PASS", 
+                    f"Checked Default Mapping for : {field} - found correct value : {expected}", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
 
 
-
+        return results_list
+    except Exception as e:
+        error_message = str(e)        
+        return [TestResult("DefaultMapping", "FAIL",f"TEST FAILED WITH EXCEPTION :  Error : {error_message[:300]}", test_from_state, inspect.stack()[0].function)]

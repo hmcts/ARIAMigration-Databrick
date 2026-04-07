8a81ef92678282e6ca6e3775f65b1b0791732e57
misbahulislam2 <misbah.ul-islam@hmcts.net>
2026-04-07 15:52:07 +0000

    fixed tab

--- tests/active/listing/listing_test.py
+++ tests/active/listing/listing_test.py
@@ -295,153 +295,153 @@ class TestListingState():
                 ("6", "UN", "FT", None, 0, 0, False, 0)    # Not AIP or LR Case
             ]
 
-        dcs_schema = T.StructType([
-        T.StructField("Detained", T.IntegerType(), True),
-        T.StructField("DetentionCentreId", T.IntegerType(), True),
-        T.StructField("DetentionCentre", T.StringType(), True),
-        T.StructField("prisonName", T.StringType(), True),
-        T.StructField("ircName", T.StringType(), True),
-        T.StructField("detentionBuilding", T.StringType(), True),
-        T.StructField("detentionAddressLines", T.StringType(), True),
-        T.StructField("detentionPostcode", T.StringType(), True),
-        T.StructField("hearingCentre", T.StringType(), True),
-        T.StructField("staffLocation", T.StringType(), True),
+                dcs_schema = T.StructType([
+                T.StructField("Detained", T.IntegerType(), True),
+                T.StructField("DetentionCentreId", T.IntegerType(), True),
+                T.StructField("DetentionCentre", T.StringType(), True),
+                T.StructField("prisonName", T.StringType(), True),
+                T.StructField("ircName", T.StringType(), True),
+                T.StructField("detentionBuilding", T.StringType(), True),
+                T.StructField("detentionAddressLines", T.StringType(), True),
+                T.StructField("detentionPostcode", T.StringType(), True),
+                T.StructField("hearingCentre", T.StringType(), True),
+                T.StructField("staffLocation", T.StringType(), True),
 
-        # caseManagementLocation as STRUCT
-        T.StructField(
-            "caseManagementLocation",
-            T.StructType([
-                T.StructField("region", T.StringType(), True),
-                T.StructField("baseLocation", T.StringType(), True),
-            ]),
-            True
-        ),
+                # caseManagementLocation as STRUCT
+                T.StructField(
+                    "caseManagementLocation",
+                    T.StructType([
+                        T.StructField("region", T.StringType(), True),
+                        T.StructField("baseLocation", T.StringType(), True),
+                    ]),
+                    True
+                ),
 
-        T.StructField("locationCode", T.StringType(), True),
-        T.StructField("locationLabel", T.StringType(), True),
-        T.StructField("selectedHearingCentreRefData", T.StringType(), True),
-        T.StructField("applicationChangeDesignatedHearingCentre", T.StringType(), True),
-    ])
+                T.StructField("locationCode", T.StringType(), True),
+                T.StructField("locationLabel", T.StringType(), True),
+                T.StructField("selectedHearingCentreRefData", T.StringType(), True),
+                T.StructField("applicationChangeDesignatedHearingCentre", T.StringType(), True),
+            ])
 
-    dcs_data = [
-    (
-        1, 2, "Rochester", "Rochester", None,
-        "HMP/YOI Rochester",
-        "1 Fort Road, Rochester, Kent",
-        "ME1 3QS",
-        "taylorHouse",
-        "Taylor House",
-        {"region": "1", "baseLocation": "765324"},
-        "765324",
-        "Taylor House Tribunal Hearing Centre",
-        "Taylor House Tribunal Hearing Centre",
-        "taylorHouse"
-    ),
-    (
-        1, 5, "Wormwood Scrubs", "Wormwood Scrubs", None,
-        "HMP Wormwood Scrubs",
-        "PO Box 757, Du Cane Road, London",
-        "W12 0AE",
-        "hattonCross",
-        "Hatton Cross",
-        {"region": "1", "baseLocation": "386417"},
-        "386417",
-        "Hatton Cross Tribunal Hearing Centre",
-        "Hatton Cross Tribunal Hearing Centre",
-        "hattonCross"
-    ),
-    (
-        1, 8, "Belmarsh", "Belmarsh", None,
-        "HMP Belmarsh",
-        "Western Way, Thamesmead, London",
-        "SE28 0EB",
-        "taylorHouse",
-        "Taylor House",
-        {"region": "1", "baseLocation": "765324"},
-        "765324",
-        "Taylor House Tribunal Hearing Centre",
-        "Taylor House Tribunal Hearing Centre",
-        "taylorHouse"
-    ),
-    (
-        1, 9, "Birmingham", "Birmingham", None,
-        "HMP Birmingham",
-        "Winson Green Road, Birmingham",
-        "B18 4AS",
-        "birmingham",
-        "Birmingham",
-        {"region": "1", "baseLocation": "231596"},
-        "231596",
-        "Birmingham Civil And Family Justice Centre",
-        "Birmingham Civil And Family Justice Centre",
-        "birmingham"
-    ),
-    (
-        1, 10, "High Down", "High Down", None,
-        "HMP/YOI High Down",
-        "High Down Lane, Sutton, Surrey",
-        "SM2 5PJ",
-        "hattonCross",
-        "Hatton Cross",
-        {"region": "1", "baseLocation": "386417"},
-        "386417",
-        "Hatton Cross Tribunal Hearing Centre",
-        "Hatton Cross Tribunal Hearing Centre",
-        "hattonCross"
-    ),
-    (
-        1, 13, "Pentonville", "Pentonville", None,
-        "HMP/YOI Pentonville",
-        "Caledonian Road, London",
-        "N7 8TT",
-        "taylorHouse",
-        "Taylor House",
-        {"region": "1", "baseLocation": "765324"},
-        "765324",
-        "Taylor House Tribunal Hearing Centre",
-        "Taylor House Tribunal Hearing Centre",
-        "taylorHouse"
-    ),
-    (
-        1, 14, "Wandsworth", "Wandsworth", None,
-        "HMP Wandsworth",
-        "PO Box 757, Heathfield Road, Wandsworth, London",
-        "SW18 3HS",
-        "hattonCross",
-        "Hatton Cross",
-        {"region": "1", "baseLocation": "386417"},
-        "386417",
-        "Hatton Cross Tribunal Hearing Centre",
-        "Hatton Cross Tribunal Hearing Centre",
-        "hattonCross"
-    ),
-    (
-        1, 18, "Elmley", "Elmley", None,
-        "HMP/YOI Elmley",
-        "Church Road, Eastchurch, Sheerness, Kent",
-        "ME12 4DZ",
-        "taylorHouse",
-        "Taylor House",
-        {"region": "1", "baseLocation": "765324"},
-        "765324",
-        "Taylor House Tribunal Hearing Centre",
-        "Taylor House Tribunal Hearing Centre",
-        "taylorHouse"
-    ),
-    (
-        1, 23, "Magilligan", "Magilligan", None,
-        "HMP Magilligan",
-        "Point Road, Londonderry, Limavady",
-        "BT49 0LR",
-        "glasgow",
-        "Glasgow",
-        {"region": "1", "baseLocation": "366559"},
-        "366559",
-        "Atlantic Quay - Glasgow",
-        "Atlantic Quay - Glasgow",
-        "glasgow"
-    ),
-    ]
+            dcs_data = [
+            (
+                1, 2, "Rochester", "Rochester", None,
+                "HMP/YOI Rochester",
+                "1 Fort Road, Rochester, Kent",
+                "ME1 3QS",
+                "taylorHouse",
+                "Taylor House",
+                {"region": "1", "baseLocation": "765324"},
+                "765324",
+                "Taylor House Tribunal Hearing Centre",
+                "Taylor House Tribunal Hearing Centre",
+                "taylorHouse"
+            ),
+            (
+                1, 5, "Wormwood Scrubs", "Wormwood Scrubs", None,
+                "HMP Wormwood Scrubs",
+                "PO Box 757, Du Cane Road, London",
+                "W12 0AE",
+                "hattonCross",
+                "Hatton Cross",
+                {"region": "1", "baseLocation": "386417"},
+                "386417",
+                "Hatton Cross Tribunal Hearing Centre",
+                "Hatton Cross Tribunal Hearing Centre",
+                "hattonCross"
+            ),
+            (
+                1, 8, "Belmarsh", "Belmarsh", None,
+                "HMP Belmarsh",
+                "Western Way, Thamesmead, London",
+                "SE28 0EB",
+                "taylorHouse",
+                "Taylor House",
+                {"region": "1", "baseLocation": "765324"},
+                "765324",
+                "Taylor House Tribunal Hearing Centre",
+                "Taylor House Tribunal Hearing Centre",
+                "taylorHouse"
+            ),
+            (
+                1, 9, "Birmingham", "Birmingham", None,
+                "HMP Birmingham",
+                "Winson Green Road, Birmingham",
+                "B18 4AS",
+                "birmingham",
+                "Birmingham",
+                {"region": "1", "baseLocation": "231596"},
+                "231596",
+                "Birmingham Civil And Family Justice Centre",
+                "Birmingham Civil And Family Justice Centre",
+                "birmingham"
+            ),
+            (
+                1, 10, "High Down", "High Down", None,
+                "HMP/YOI High Down",
+                "High Down Lane, Sutton, Surrey",
+                "SM2 5PJ",
+                "hattonCross",
+                "Hatton Cross",
+                {"region": "1", "baseLocation": "386417"},
+                "386417",
+                "Hatton Cross Tribunal Hearing Centre",
+                "Hatton Cross Tribunal Hearing Centre",
+                "hattonCross"
+            ),
+            (
+                1, 13, "Pentonville", "Pentonville", None,
+                "HMP/YOI Pentonville",
+                "Caledonian Road, London",
+                "N7 8TT",
+                "taylorHouse",
+                "Taylor House",
+                {"region": "1", "baseLocation": "765324"},
+                "765324",
+                "Taylor House Tribunal Hearing Centre",
+                "Taylor House Tribunal Hearing Centre",
+                "taylorHouse"
+            ),
+            (
+                1, 14, "Wandsworth", "Wandsworth", None,
+                "HMP Wandsworth",
+                "PO Box 757, Heathfield Road, Wandsworth, London",
+                "SW18 3HS",
+                "hattonCross",
+                "Hatton Cross",
+                {"region": "1", "baseLocation": "386417"},
+                "386417",
+                "Hatton Cross Tribunal Hearing Centre",
+                "Hatton Cross Tribunal Hearing Centre",
+                "hattonCross"
+            ),
+            (
+                1, 18, "Elmley", "Elmley", None,
+                "HMP/YOI Elmley",
+                "Church Road, Eastchurch, Sheerness, Kent",
+                "ME12 4DZ",
+                "taylorHouse",
+                "Taylor House",
+                {"region": "1", "baseLocation": "765324"},
+                "765324",
+                "Taylor House Tribunal Hearing Centre",
+                "Taylor House Tribunal Hearing Centre",
+                "taylorHouse"
+            ),
+            (
+                1, 23, "Magilligan", "Magilligan", None,
+                "HMP Magilligan",
+                "Point Road, Londonderry, Limavady",
+                "BT49 0LR",
+                "glasgow",
+                "Glasgow",
+                {"region": "1", "baseLocation": "366559"},
+                "366559",
+                "Atlantic Quay - Glasgow",
+                "Atlantic Quay - Glasgow",
+                "glasgow"
+            ),
+            ]
 
             silver_m1_test_data = spark.createDataFrame(m1_data, self.M1_COLUMNS)
             silver_m2_test_data = spark.createDataFrame([], self.CASE_NO_COLUMNS)

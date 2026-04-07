4ec71441a3cfd78b671a05ee18f09a947f9f0944
misbahulislam2 <misbah.ul-islam@hmcts.net>
2026-04-07 15:10:37 +0000

    fixed decision and decided_b unit tests

--- tests/active/decided_b/general_test.py
+++ tests/active/decided_b/general_test.py
@@ -52,14 +52,17 @@ def general_outputs(spark):
     T.StructField("CaseNo", T.StringType(), True),
     T.StructField("Appellant_Name", T.StringType(), True),
     T.StructField("Appellant_Postcode", T.StringType(), True),
-    T.StructField("Relationship", T.StringType(), True)])
+    T.StructField("Relationship", T.StringType(), True),
+    T.StructField("DetentionCentreId", T.IntegerType(), True),
+    T.StructField("Detained", T.IntegerType(), True),
+    ])
     
     m2_data = [
-        ("CASE001", "Gold", "B12 0hf","Relationship1"),  
-        ("CASE002", "Smith", "M8 1XY","Relationship2"),  
-        ("CASE003", "Johns", "DE4 9HN","Relationship3"),  
-        ("CASE004", "Black", "BN6 0PA","Relationship4"),  
-        ("CASE005", "Green", "DD7 7PT",None), 
+        ("CASE001", "Gold", "B12 0hf","Relationship1"2,1),  
+        ("CASE002", "Smith", "M8 1XY","Relationship2"5,2),  
+        ("CASE003", "Johns", "DE4 9HN","Relationship3"8,3),  
+        ("CASE004", "Black", "BN6 0PA","Relationship4"9,4),  
+        ("CASE005", "Green", "DD7 7PT",None,9,4), 
         ]
 
     m3_schema = T.StructType([
@@ -140,6 +143,154 @@ def general_outputs(spark):
         ("Coventry Centre", None, "Court5","Nor","ply"),   
         ("London Centre", "xyz", "Court6","ply","Nor"),  
         ]
+    dcs_schema = T.StructType([
+        T.StructField("Detained", T.IntegerType(), True),
+        T.StructField("DetentionCentreId", T.IntegerType(), True),
+        T.StructField("DetentionCentre", T.StringType(), True),
+        T.StructField("prisonName", T.StringType(), True),
+        T.StructField("ircName", T.StringType(), True),
+        T.StructField("detentionBuilding", T.StringType(), True),
+        T.StructField("detentionAddressLines", T.StringType(), True),
+        T.StructField("detentionPostcode", T.StringType(), True),
+        T.StructField("hearingCentre", T.StringType(), True),
+        T.StructField("staffLocation", T.StringType(), True),
+
+        # caseManagementLocation as STRUCT
+        T.StructField(
+            "caseManagementLocation",
+            T.StructType([
+                T.StructField("region", T.StringType(), True),
+                T.StructField("baseLocation", T.StringType(), True),
+            ]),
+            True
+        ),
+
+        T.StructField("locationCode", T.StringType(), True),
+        T.StructField("locationLabel", T.StringType(), True),
+        T.StructField("selectedHearingCentreRefData", T.StringType(), True),
+        T.StructField("applicationChangeDesignatedHearingCentre", T.StringType(), True),
+    ])
+
+    dcs_data = [
+    (
+        1, 2, "Rochester", "Rochester", None,
+        "HMP/YOI Rochester",
+        "1 Fort Road, Rochester, Kent",
+        "ME1 3QS",
+        "taylorHouse",
+        "Taylor House",
+        {"region": "1", "baseLocation": "765324"},
+        "765324",
+        "Taylor House Tribunal Hearing Centre",
+        "Taylor House Tribunal Hearing Centre",
+        "taylorHouse"
+    ),
+    (
+        1, 5, "Wormwood Scrubs", "Wormwood Scrubs", None,
+        "HMP Wormwood Scrubs",
+        "PO Box 757, Du Cane Road, London",
+        "W12 0AE",
+        "hattonCross",
+        "Hatton Cross",
+        {"region": "1", "baseLocation": "386417"},
+        "386417",
+        "Hatton Cross Tribunal Hearing Centre",
+        "Hatton Cross Tribunal Hearing Centre",
+        "hattonCross"
+    ),
+    (
+        1, 8, "Belmarsh", "Belmarsh", None,
+        "HMP Belmarsh",
+        "Western Way, Thamesmead, London",
+        "SE28 0EB",
+        "taylorHouse",
+        "Taylor House",
+        {"region": "1", "baseLocation": "765324"},
+        "765324",
+        "Taylor House Tribunal Hearing Centre",
+        "Taylor House Tribunal Hearing Centre",
+        "taylorHouse"
+    ),
+    (
+        1, 9, "Birmingham", "Birmingham", None,
+        "HMP Birmingham",
+        "Winson Green Road, Birmingham",
+        "B18 4AS",
+        "birmingham",
+        "Birmingham",
+        {"region": "1", "baseLocation": "231596"},
+        "231596",
+        "Birmingham Civil And Family Justice Centre",
+        "Birmingham Civil And Family Justice Centre",
+        "birmingham"
+    ),
+    (
+        1, 10, "High Down", "High Down", None,
+        "HMP/YOI High Down",
+        "High Down Lane, Sutton, Surrey",
+        "SM2 5PJ",
+        "hattonCross",
+        "Hatton Cross",
+        {"region": "1", "baseLocation": "386417"},
+        "386417",
+        "Hatton Cross Tribunal Hearing Centre",
+        "Hatton Cross Tribunal Hearing Centre",
+        "hattonCross"
+    ),
+    (
+        1, 13, "Pentonville", "Pentonville", None,
+        "HMP/YOI Pentonville",
+        "Caledonian Road, London",
+        "N7 8TT",
+        "taylorHouse",
+        "Taylor House",
+        {"region": "1", "baseLocation": "765324"},
+        "765324",
+        "Taylor House Tribunal Hearing Centre",
+        "Taylor House Tribunal Hearing Centre",
+        "taylorHouse"
+    ),
+    (
+        1, 14, "Wandsworth", "Wandsworth", None,
+        "HMP Wandsworth",
+        "PO Box 757, Heathfield Road, Wandsworth, London",
+        "SW18 3HS",
+        "hattonCross",
+        "Hatton Cross",
+        {"region": "1", "baseLocation": "386417"},
+        "386417",
+        "Hatton Cross Tribunal Hearing Centre",
+        "Hatton Cross Tribunal Hearing Centre",
+        "hattonCross"
+    ),
+    (
+        1, 18, "Elmley", "Elmley", None,
+        "HMP/YOI Elmley",
+        "Church Road, Eastchurch, Sheerness, Kent",
+        "ME12 4DZ",
+        "taylorHouse",
+        "Taylor House",
+        {"region": "1", "baseLocation": "765324"},
+        "765324",
+        "Taylor House Tribunal Hearing Centre",
+        "Taylor House Tribunal Hearing Centre",
+        "taylorHouse"
+    ),
+    (
+        1, 23, "Magilligan", "Magilligan", None,
+        "HMP Magilligan",
+        "Point Road, Londonderry, Limavady",
+        "BT49 0LR",
+        "glasgow",
+        "Glasgow",
+        {"region": "1", "baseLocation": "366559"},
+        "366559",
+        "Atlantic Quay - Glasgow",
+        "Atlantic Quay - Glasgow",
+        "glasgow"
+    ),
+    ]
+
     
 
     df_m1 =  spark.createDataFrame(m1_data, m1_schema)
@@ -148,8 +299,9 @@ def general_outputs(spark):
     df_mh =  spark.createDataFrame(mh_data, mh_schema)
     df_bhc =  spark.createDataFrame(bhc_data, bhc_schema)
     df_bdhc =  spark.createDataFrame(bdhc_data, bdhc_schema)
+    df_dcs =  spark.createDataFrame(dcs_data, dcs_schema)
 
-    general_content,_ = general(df_m1, df_m2, df_m3, df_mh, df_bhc, df_bdhc)
+    general_content,_ = general(df_m1, df_m2, df_m3, df_mh, df_bhc, df_bdhc,df_dcs)
     results = {row["CaseNo"]: row.asDict() for row in general_content.collect()}
     
     return results

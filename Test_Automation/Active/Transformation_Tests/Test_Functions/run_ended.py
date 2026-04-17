0df50da43d0d81e82268184609dca933e0810b57
rafalam1 <Rafiul.Alam@HMCTS.NET>
2026-04-17 14:56:11 +0000

    ended transformation tests

--- Test_Automation/Active/Transformation_Tests/Test_Functions/run_ended.py
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/run_ended.py
@@ -7,11 +7,38 @@ def run_all_tests(json_data, M1_bronze, M1_silver, M2_bronze, M3_bronze, M3_silv
     all_test_results = []
 
     # -- Default mappings --
-    test_df, test_data_setup = ended_tests.test_default_mapping_init(json_data, M3_bronze)
-    if test_data_setup != True:
-        all_test_results.append(test_data_setup)
+    # test_df, test_data_setup = ended_tests.test_default_mapping_init(json_data, M3_bronze)
+    # if test_data_setup != True:
+    #     all_test_results.append(test_data_setup)
 
-    if test_df is not None:
-        all_test_results.extend(ended_tests.test_ended_defaultValues(test_df, fields_to_exclude))
+    # if test_df is not None:
+    #     all_test_results.extend(ended_tests.test_ended_defaultValues(test_df, fields_to_exclude))
+
+    # -- caseData Tests --
+    test_data_setup = None
+    test_df, test_data_setup =  ended_tests.test_caseData_init(json_data, M1_bronze, M3_bronze)
+    if test_data_setup != True:
+         all_test_results.append(test_data_setup)
+
+    if test_df != None:
+         if "outOfTimeDecisionType" not in fields_to_exclude:
+              all_test_results.append(ended_tests.test_outOfTimeDecisionType_test1(test_df))
+              all_test_results.append(ended_tests.test_outOfTimeDecisionType_test2(test_df))
+              all_test_results.append(ended_tests.test_outOfTimeDecisionType_test3(test_df))
+
+    return classify_all(all_test_results)
+
+    # -- hearingRequirements Tests --
+    test_data_setup = None
+    test_df, test_data_setup =  ended_tests.test_hearingRequirements_init(json_data, M1_bronze, M3_bronze)
+    if test_data_setup != True:
+         all_test_results.append(test_data_setup)
+
+    if test_df != None:
+         if "isEvidenceFromOutsideUkOoc" not in fields_to_exclude:
+              all_test_results.append(ended_tests.test_isEvidenceFromOutsideUkOoc_test1(test_df))
+              all_test_results.append(ended_tests.test_isEvidenceFromOutsideUkOoc_test2(test_df))
+              all_test_results.append(ended_tests.test_isEvidenceFromOutsideUkOoc_test3(test_df))
+              all_test_results.append(ended_tests.test_isEvidenceFromOutsideUkOoc_test4(test_df))
 
     return classify_all(all_test_results)

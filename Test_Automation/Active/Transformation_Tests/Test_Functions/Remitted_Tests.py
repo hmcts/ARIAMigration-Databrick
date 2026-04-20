3cf52b0f24b834e4e48c2cf23724315cd72fb32c
NazeeKadiu <nazee.kadiu1@hmcts.net>
2026-04-20 08:20:41 +0000

    Add remitted default mapping tests

--- Test_Automation/Active/Transformation_Tests/Test_Functions/Remitted_Tests.py
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/Remitted_Tests.py
@@ -4,6 +4,7 @@ from pyspark.sql.functions import (
     size, udf, coalesce, concat_ws, concat, trim, year, split, datediff,
     collect_set, current_timestamp,transform, first, array_contains
 )
+
 import inspect
 
 #Import Test Results class
@@ -12,11 +13,65 @@ from models.test_result import TestResult
 #Temp solution : using variable below, when each testresult instance is created, to tag with where test run from
 test_from_state = "remitted"
 
-def testcase1():    
-    return TestResult("testcasefield", "FAIL", f"remitted - test 1 complete", test_from_state, inspect.stack()[0].function)
+############################################################################################
+#######################
+# default mapping Init code
+#######################
 
-def testcase2():    
-    return TestResult("testcasefield", "PASS", f"remitted - test 2 complete", test_from_state, inspect.stack()[0].function)
+def test_default_mapping_init(json):
+    try:
+        test_df = json.select(
+            "rehearingReason",
+            "sourceOfRemittal",
+            "courtReferenceNumber",
+            "caseFlagSetAsideReheardExists",
+            "remittalDocuments",
+            "uploadOtherRemittalDocs"
+        )
+        return test_df, True
+    except Exception as e:
+        error_message = str(e)        
+        return None,TestResult("DefaultMapping", "FAIL",f"Failed to Setup Data for Test : Error : {error_message[:300]}", test_from_state, inspect.stack()[0].function)
+
+def test_remitted_defaultValues(test_df, fields_to_exclude):
+    try:
+        expected_defaults = {
+            "rehearingReason": "Remitted",
+            "sourceOfRemittal": "Upper Tribunal",
+            "courtReferenceNumber": "This is a migrated ARIA case. Please refer to the documents.",
+            "caseFlagSetAsideReheardExists": "Yes"
+        }
+
+        expected_arrays = {
+           "remittalDocuments": None,
+           "uploadOtherRemittalDocs": None
+        }
+        
+        results_list = []
+
+        for field, expected in expected_defaults.items():
+            if field in fields_to_exclude:
+                continue
+            condition = (col(field) != expected)
+            if test_df.filter(condition).count() > 0:
+                results_list.append(TestResult(
+                    field, 
+                    "FAIL", 
+                    f"Failed to check Default Mapping for : {field} - expected : {expected} - found {str(test_df.filter(condition).count())} records not matching", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
+            else:
+                results_list.append(TestResult(
+                    field, 
+                    "PASS", 
+                    f"Checked Default Mapping for : {field} - found correct value : {expected}", 
+                    test_from_state,
+                    inspect.stack()[0].function
+                ))
 
 
-
+        return results_list
+    except Exception as e:
+        error_message = str(e)        
+        return [TestResult("DefaultMapping", "FAIL",f"TEST FAILED WITH EXCEPTION :  Error : {error_message[:300]}", test_from_state, inspect.stack()[0].function)]
--- Test_Automation/Active/Transformation_Tests/Test_Functions/run_remitted.py
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/run_remitted.py
@@ -6,7 +6,15 @@ from Test_Functions.test_helpers import classify_all
 def run_all_tests(json_data, M1_bronze, M1_silver, M2_bronze, M3_bronze, C, bhc, fields_to_exclude):
     all_test_results = []
 
-    all_test_results.append(rem_tests.testcase1())
-    all_test_results.append(rem_tests.testcase2())
+
+    # -- Default Mappings --
+    test_data_setup = None
+    test_df, test_data_setup = rem_tests.test_default_mapping_init(json_data)
+    if test_data_setup != True:
+         all_test_results.append(test_data_setup)
+
+    if test_df != None:
+        all_test_results.extend(rem_tests.test_remitted_defaultValues(test_df, fields_to_exclude))
+    # display(all_test_results)
 
     return classify_all(all_test_results)

efddaba1491c17b3af53b5bbcc804610e42cf664
rafalam1 <Rafiul.Alam@HMCTS.NET>
2026-06-05 09:12:25 +0000

    fields to exclude

--- Test_Automation/Active/Transformation_Tests/Test_Functions/run_appealSubmitted.py
+++ Test_Automation/Active/Transformation_Tests/Test_Functions/run_appealSubmitted.py
@@ -6,80 +6,84 @@ from Test_Functions.test_helpers import classify_all
 def run_all_tests(json_data, M1_bronze, M1_silver, M2_bronze, M3_bronze, C, bhc, bat, bhoref, external_storage, spark, fields_to_exclude, M4_silver=None, M4_bronze=None):
     all_test_results = []
 
-    # -- Default mappings --
-    test_data_setup = None
-    test_df, test_data_setup =  as_tests.test_default_mapping_init(json_data, M1_silver)
-    if test_data_setup != True:
-         all_test_results.append(test_data_setup)
+    # # -- Default mappings --
+    # test_data_setup = None
+    # test_df, test_data_setup =  as_tests.test_default_mapping_init(json_data, M1_silver)
+    # if test_data_setup != True:
+    #      all_test_results.append(test_data_setup)
 
-    if test_df != None:
-        all_test_results.extend(as_tests.test_AS_defaultValues(test_df,fields_to_exclude))
-    #display(all_test_results)
+    # if test_df != None:
+    #     all_test_results.extend(as_tests.test_AS_defaultValues(test_df,fields_to_exclude))
+    # #display(all_test_results)
 
     # -- Home Office Mappings (DIAC-2371) --
     ho_test_setup = None
-    ho_test_df, ho_test_setup = as_tests.test_ho_mapping_init(json_data, M1_silver)
-    if ho_test_setup != True:
-        all_test_results.append(ho_test_setup)
+    
+    if "homeOfficeSearchStatus" not in fields_to_exclude:
+        ho_test_df, ho_test_setup = as_tests.test_ho_mapping_init(json_data, M1_silver)
+        if ho_test_setup != True:
+            all_test_results.append(ho_test_setup)
 
-    if ho_test_df != None:
-        all_test_results.extend(as_tests.test_AS_homeOfficeDefaultValues(ho_test_df, fields_to_exclude))
+        if ho_test_df != None:
+            all_test_results.extend(as_tests.test_AS_homeOfficeDefaultValues(ho_test_df, fields_to_exclude))
+    else:
+        print("Skipping Home Office Init and Test entirely because fields are excluded for this state.")
 
 
-    # -- Payment --
-    test_data_setup = None
-    test_df, test_data_setup =  as_tests.test_payment_init(json_data, M1_bronze, M4_silver)
-    if test_data_setup != True:
-         all_test_results.append(test_data_setup)
+    # # -- Payment --
+    # test_data_setup = None
+    # test_df, test_data_setup =  as_tests.test_payment_init(json_data, M1_bronze, M4_silver)
+    # if test_data_setup != True:
+    #      all_test_results.append(test_data_setup)
 
-    if test_df != None:
-        if "paymentStatus" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_paymentStatus_test1(test_df))
-            all_test_results.append(as_tests.test_paymentStatus_test2(test_df))
+    # if test_df != None:
+    #     if "paymentStatus" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_paymentStatus_test1(test_df))
+    #         all_test_results.append(as_tests.test_paymentStatus_test2(test_df))
 
-        if "rpDcAppealHearingOption" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_rpDcAppealHearingOption_test1(test_df))
-            all_test_results.append(as_tests.test_rpDcAppealHearingOption_test2(test_df))
-            all_test_results.append(as_tests.test_rpDcAppealHearingOption_test3(test_df))
+    #     if "rpDcAppealHearingOption" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_rpDcAppealHearingOption_test1(test_df))
+    #         all_test_results.append(as_tests.test_rpDcAppealHearingOption_test2(test_df))
+    #         all_test_results.append(as_tests.test_rpDcAppealHearingOption_test3(test_df))
 
-        if "paidDate" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_paidDate_test1(test_df))
-            all_test_results.append(as_tests.test_paidDate_test2(test_df))
+    #     if "paidDate" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_paidDate_test1(test_df))
+    #         all_test_results.append(as_tests.test_paidDate_test2(test_df))
 
-        if "paidAmount" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_paidAmount_test1(test_df))
-            all_test_results.append(as_tests.test_paidAmount_test2(test_df))
+    #     if "paidAmount" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_paidAmount_test1(test_df))
+    #         all_test_results.append(as_tests.test_paidAmount_test2(test_df))
 
-        if "paymentDescription" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_paymentDescription_test1(test_df))
-            all_test_results.append(as_tests.test_paymentDescription_test2(test_df))
-            all_test_results.append(as_tests.test_paymentDescription_test3(test_df))
+    #     if "paymentDescription" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_paymentDescription_test1(test_df))
+    #         all_test_results.append(as_tests.test_paymentDescription_test2(test_df))
+    #         all_test_results.append(as_tests.test_paymentDescription_test3(test_df))
 
-    # -- Remission --
-    test_data_setup = None
-    test_df, test_data_setup =  as_tests.test_remission_init(json_data, M1_bronze, M4_bronze)
-    if test_data_setup != True:
-         all_test_results.append(test_data_setup)
+    # # -- Remission --
+    # test_data_setup = None
+    # test_df, test_data_setup =  as_tests.test_remission_init(json_data, M1_bronze, M4_bronze)
+    # if test_data_setup != True:
+    #      all_test_results.append(test_data_setup)
 
-    if test_df != None:
-        if "remissionDecision" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_remissionDecision_test1(test_df))
-            all_test_results.append(as_tests.test_remissionDecision_test2(test_df))
-            all_test_results.append(as_tests.test_remissionDecision_test3(test_df))
+    # if test_df != None:
+    #     if "remissionDecision" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_remissionDecision_test1(test_df))
+    #         all_test_results.append(as_tests.test_remissionDecision_test2(test_df))
+    #         all_test_results.append(as_tests.test_remissionDecision_test3(test_df))
 
-        if "remissionDecisionReason" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_remissionDecisionReason_test1(test_df))
-            all_test_results.append(as_tests.test_remissionDecisionReason_test2(test_df))
-            all_test_results.append(as_tests.test_remissionDecisionReason_test3(test_df))
+    #     if "remissionDecisionReason" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_remissionDecisionReason_test1(test_df))
+    #         all_test_results.append(as_tests.test_remissionDecisionReason_test2(test_df))
+    #         all_test_results.append(as_tests.test_remissionDecisionReason_test3(test_df))
 
-        if "amountLeftToPay" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_amountLeftToPay_test1(test_df))
-            all_test_results.append(as_tests.test_amountLeftToPay_test2(test_df))
-            all_test_results.append(as_tests.test_amountLeftToPay_test3(test_df))
+    #     if "amountLeftToPay" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_amountLeftToPay_test1(test_df))
+    #         all_test_results.append(as_tests.test_amountLeftToPay_test2(test_df))
+    #         all_test_results.append(as_tests.test_amountLeftToPay_test3(test_df))
 
-        if "amountRemitted" not in fields_to_exclude:
-            all_test_results.append(as_tests.test_amountRemitted_test1(test_df))
-            all_test_results.append(as_tests.test_amountRemitted_test2(test_df))
-            all_test_results.append(as_tests.test_amountRemitted_test3(test_df))
+    #     if "amountRemitted" not in fields_to_exclude:
+    #         all_test_results.append(as_tests.test_amountRemitted_test1(test_df))
+    #         all_test_results.append(as_tests.test_amountRemitted_test2(test_df))
+    #         all_test_results.append(as_tests.test_amountRemitted_test3(test_df))
 
     return classify_all(all_test_results)

69a3aa1ddb5abd7660597889a5f68e068e76d574
Tony Chow <tony.chow@hmcts.net>
2026-06-16 17:23:57 +0100

    Updated PartyId functionality to remove unused conditions and tables

--- tests/active/paymentPending/PartyID_test.py
+++ tests/active/paymentPending/PartyID_test.py
@@ -38,41 +38,15 @@ def partyID_outputs(spark):
         ("HU/00592/2025", "LR", None, "Ghost Sponsor"),
     ]
 
-    m3_schema = T.StructType([
-        T.StructField("CaseNo", T.StringType(), True),
-        T.StructField("StatusId", T.IntegerType(), True), 
-        T.StructField("Outcome", T.IntegerType(), True),
-    ])
-
-    m3_data = [
-    ("HU/00638/2024", 1, None),
-    ("HU/00638/2024", 2, 1),
-    ("EA/00556/2025", 1, 1),
-    ("HU/01871/2024", 1, 1),
-    ("HU/00588/2025", 1, 1),
-    ("HU/00592/2025", 1, 1)]
-
-    silver_c_schema = T.StructType([
-        T.StructField("CaseNo", T.StringType(), True),
-        T.StructField("CategoryId", T.IntegerType(), True)
-    ])
-
-    silver_c_data = [
-    ("HU/00638/2024", 38),
-    ("EA/00556/2025", 38),
-    ("HU/01871/2024", 10),
-    ]
-        
-    silver_m1 =  spark.createDataFrame(m1_data, m1_schema)
-    silver_m3 =  spark.createDataFrame(m3_data, m3_schema)
-    silver_c =  spark.createDataFrame(silver_c_data, silver_c_schema)
+    silver_m1 = spark.createDataFrame(m1_data, m1_schema)
 
     # Call the function under test
-    partyID_content, _ = partyID(silver_m1, silver_m3, silver_c)
+    partyID_content, _ = partyID(silver_m1)
 
     # Convert to dictionary keyed by CaseNo
     results = {row["CaseNo"]: row.asDict() for row in partyID_content.collect()}
     return results
+
 
 def is_uuid(v):
     try:
@@ -81,31 +55,37 @@ def is_uuid(v):
     except Exception:
         return False
 
+
 def test_appellant_party_id_created(partyID_outputs):
     row = partyID_outputs["HU/00638/2024"]
     assert is_uuid(row["appellantPartyId"])
+
 
 def test_legal_rep_created_for_LR(partyID_outputs):
     row = partyID_outputs["HU/00638/2024"]
     assert is_uuid(row["legalRepIndividualPartyId"])
     assert is_uuid(row["legalRepOrganisationPartyId"])
 
+
 def test_legal_rep_null_for_AIP(partyID_outputs):
     row = partyID_outputs["EA/00556/2025"]
     assert row["legalRepIndividualPartyId"] is None
     assert row["legalRepOrganisationPartyId"] is None
 
-def test_sponsor_created_when_category_38_and_name(partyID_outputs):
+
+def test_sponsor_created_when_name_not_null(partyID_outputs):
     row = partyID_outputs["HU/00638/2024"]
     assert is_uuid(row["sponsorPartyId"])
+
 
 def test_sponsor_null_without_name(partyID_outputs):
     row = partyID_outputs["HU/01871/2024"]
     assert row["sponsorPartyId"] is None
 
+
 def test_no_ids_when_appeal_type_null(partyID_outputs):
     row = partyID_outputs["HU/00592/2025"]
-    assert is_uuid(row["appellantPartyId"]) #each case as an appealType. this should not be null
+    assert is_uuid(row["appellantPartyId"])  # each case as an appealType. this should not be null
     assert row["legalRepIndividualPartyId"] is None
     assert row["legalRepOrganisationPartyId"] is None
     assert row["sponsorPartyId"] is None

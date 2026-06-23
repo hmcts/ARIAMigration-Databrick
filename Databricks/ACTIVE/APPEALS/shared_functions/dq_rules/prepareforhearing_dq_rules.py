d2642fd031d898a08850326c07cf4dc079527329
AndrewHMCTS <Andrew.McDevitt@HMCTS.NET>
2026-06-23 15:40:48 +0000

    Fix listCase DQs
    Fixing listCaseHEaringLength, listCaseHearingDate, listCaseHearingCentre, listCaseHearingCentreAddress

--- Databricks/ACTIVE/APPEALS/shared_functions/dq_rules/prepareforhearing_dq_rules.py
+++ Databricks/ACTIVE/APPEALS/shared_functions/dq_rules/prepareforhearing_dq_rules.py
@@ -259,32 +259,62 @@ class prepareForHearingDQRules(DQRulesBase):
 
         checks["valid_listCaseHearingLength"] = ("""
         (
-            CAST(roundedTimeEstimate AS STRING) <=> CAST(listCaseHearingLength AS STRING) 
-            AND CaseStatus_dec IN (37,38)
-            AND CAST(roundedTimeEstimate AS INT) IN (30, 60, 90, 120, 150, 180,210, 240, 270, 300, 330, 360)
+            (
+                CAST(roundedTimeEstimate AS STRING) <=> CAST(listCaseHearingLength AS STRING) 
+                AND CaseStatus_dec IN (37,38)
+                AND CAST(roundedTimeEstimate AS INT) IN (30, 60, 90, 120, 150, 180,210, 240, 270, 300, 330, 360)
+            )
+            OR
+            (
+                (CaseStatus_dec NOT IN (37,38) OR CaseStatus_dec IS NULL) AND roundedTimeEstimate IS NULL
+            )
         )
         """)
 
         checks["valid_listCaseHearingDate"] = (
             """
             (
-                listCaseHearingDate <=>
-                    CONCAT(date_format(CAST(HearingDate AS timestamp), 'yyyy-MM-dd'),'T',
-                        CASE
-                        WHEN StartTime IS NULL THEN '00:00:00.000'
-                        ELSE date_format(CAST(StartTime AS timestamp), 'HH:mm:ss.SSS')
-                        END)
-                    AND CaseStatus_dec IN (37,38)
+                (
+                    listCaseHearingDate <=>
+                        CONCAT(date_format(CAST(HearingDate AS timestamp), 'yyyy-MM-dd'),'T',
+                            CASE
+                            WHEN StartTime IS NULL THEN '00:00:00.000'
+                            ELSE date_format(CAST(StartTime AS timestamp), 'HH:mm:ss.SSS')
+                            END)
+                        AND CaseStatus_dec IN (37,38)
+                )
+                OR
+                (
+                    (CaseStatus_dec NOT IN (37,38) OR CaseStatus_dec IS NULL) AND roundedTimeEstimate IS NULL
+                )
             )
             """)
 
         checks["valid_listCaseHearingCentre"] = (
-            "(listCaseHearingCentre <=> bronze_listCaseHearingCentre AND CaseStatus_dec IN (37,38) )" 
-        )
+            """
+            (
+                (
+                    listCaseHearingCentre <=> bronze_listCaseHearingCentre AND CaseStatus_dec IN (37,38) 
+                )
+                OR
+                (
+                    (CaseStatus_dec NOT IN (37,38) OR CaseStatus_dec IS NULL) AND listCaseHearingCentre IS NULL
+                )
+            )
+        """)
 
         checks["valid_listCaseHearingCentreAddress"] = (
-            "(listCaseHearingCentreAddress <=> bronze_listCaseHearingCentreAddress AND CaseStatus_dec IN (37,38))"
-        )
+            """
+            (
+                (
+                    listCaseHearingCentreAddress <=> bronze_listCaseHearingCentreAddress AND CaseStatus_dec IN (37,38)
+                )
+                OR
+                (
+                    (CaseStatus_dec NOT IN (37,38) OR CaseStatus_dec IS NULL) AND listCaseHearingCentreAddress IS NULL
+                )
+            )
+            """)
 
         return checks
 
